dcb31d1500d96b3e301a620fc8a4756d883b8493
Carlos-93 <carlos93.bcn@hotmail.com>
2024-04-12 16:05:45 +0200

    update

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -3,7 +3,7 @@ import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angula
 import { Title } from '@angular/platform-browser';
 
 // Interface para el formulario de productos
-export interface Product {
+export interface product {
   reference: string;
   name: string;
   price: number;
@@ -29,7 +29,7 @@ export class FormComponent implements OnInit {
   ngOnInit() { this.titleService.setTitle('Apple (UK) - Admin Dashboard'); }
 
   // Creamos un objeto del tipo de la inteface para guardar los datos en él y lo inicializamos
-  product: Product = {
+  product: product = {
     reference: '',
     name: '',
     price: 0,
@@ -40,7 +40,7 @@ export class FormComponent implements OnInit {
   };
 
   // Formulario para insertar un nuevo producto
-  ProductForm = new FormGroup({
+  productForm = new FormGroup({
     reference: new FormControl('', [Validators.required, Validators.minLength(1)]),
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     price: new FormControl('', [Validators.required, Validators.min(0)]),
@@ -54,17 +54,17 @@ export class FormComponent implements OnInit {
   onSubmit() {
 
     // Si el formulario es válido, guardamos los datos en el objeto product
-    if (this.ProductForm.valid) {
-      this.product.reference = this.ProductForm.value.reference ? this.ProductForm.value.reference : '';
-      this.product.name = this.ProductForm.value.name ? this.ProductForm.value.name : '';
-      this.product.price = this.ProductForm.value.price ? Number(this.ProductForm.value.price) : 0;
-      this.product.description = this.ProductForm.value.description ? this.ProductForm.value.description : '';
-      this.product.type = this.ProductForm.value.type ? this.ProductForm.value.type : '';
-      this.product.offer = this.ProductForm.value.offer ? Boolean(this.ProductForm.value.offer) : false;
-      this.product.image = this.ProductForm.value.image ? this.ProductForm.value.image : '';
+    if (this.productForm.valid) {
+      this.product.reference = this.productForm.value.reference ? this.productForm.value.reference : '';
+      this.product.name = this.productForm.value.name ? this.productForm.value.name : '';
+      this.product.price = this.productForm.value.price ? Number(this.productForm.value.price) : 0;
+      this.product.description = this.productForm.value.description ? this.productForm.value.description : '';
+      this.product.type = this.productForm.value.type ? this.productForm.value.type : '';
+      this.product.offer = this.productForm.value.offer ? Boolean(this.productForm.value.offer) : false;
+      this.product.image = this.productForm.value.image ? this.productForm.value.image : '';
 
       // Reiniciamos el formulario
-      this.ProductForm.reset();
+      this.productForm.reset();
     }
   }
 }
