e924aeb1c0625bcee47bb9df3cfb6ff56129413e
Carlos-93 <carlos93.bcn@hotmail.com>
2024-04-21 21:06:50 +0200

    update

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,58 +2,43 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule, ValidatorFn, AbstractControl } from '@angular/forms';
 import { ProductsService } from '../../services/products-service';
+import { PopupComponent } from '../../components/popup/popup.component';
 import { Product } from '../../interfaces/product';
 import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, PopupComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  // Almacenamos el estado de éxito del modal
-  success: boolean = false;
-
-  // Almacenamos en products el array de productos Apple ubicado en el servicio
+  productForm: FormGroup;
   products = this.ProductsService.products;
+  isEditing: boolean = false;
+  showPopup: boolean = false;
+  popupMessage: string = '';
 
-  // Inicializamos el objeto de la interfaz con los valores por defecto
-  product: Product = {
-    reference: '',
-    name: '',
-    price: 0,
-    description: '',
-    type: '',
-    offer: false,
-    image: ''
-  };
-
-  // Configuramos el formulario con validaciones para cada campo
-  productForm = new FormGroup({
-    reference: new FormControl('', { validators: [Validators.required, Validators.minLength(1), Validators.maxLength(20), this.checkUniqueReference()] }),
-    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
-    price: new FormControl(0, [Validators.required, Validators.min(0), Validators.pattern(/^[0-9]+(\.[0-9]{1,2})?$/)]),
-    description: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(500)]),
-    type: new FormControl('', [Validators.required]),
-    offer: new FormControl(false),
-    image: new FormControl('', [Validators.required, Validators.pattern(/\.(jpg|jpeg|png)$/i)])
-  });
-
-  // Inyectamos el servicio Title y ProductsService para usar en el componente
-  constructor(private titleService: Title, private ProductsService: ProductsService) { }
-
-  ngOnInit(): void {
-    // Establecemos el título de la página usando el servicio Title
-    this.titleService.setTitle('Apple (UK) - Admin Dashboard');
-    // Obtenemos todos los productos y almacenamos los resultados en 'products'
-    this.products = [...this.ProductsService.shareData()];
+  constructor(private titleService: Title, private ProductsService: ProductsService) {
+    this.productForm = new FormGroup({
+      reference: new FormControl('', { validators: [Validators.required, Validators.minLength(1), Validators.maxLength(20), this.uniqueReference()] }),
+      name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+      price: new FormControl('', [Validators.required, Validators.min(0), Validators.pattern(/^[0-9]+(\.[0-9]{1,2})?$/)]),
+      description: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(500)]),
+      type: new FormControl('', [Validators.required]),
+      offer: new FormControl(false),
+      image: new FormControl('', [Validators.required, Validators.pattern(/\.(jpg|jpeg|png)$/i)])
+    });
   }
 
-  // Método para que la referencia del producto empiece por un número
-  checkUniqueReference(): ValidatorFn {
+  product: Product = {
+    reference: '', name: '', price: 0, description: '', type: '', offer: false, image: ''
+  };
+
+  // Método para que la referencia del producto empiece por un número (Validador personalizado)
+  uniqueReference(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
       const reference = control.value;
       if (reference && !/^[0-9]/.test(reference)) {
@@ -65,9 +50,10 @@ export class DashboardComponent implements OnInit {
 
   // Método para comprobar si la referencia del producto ya existe
   checkReference() {
-    const reference = this.productForm.get('reference')?.value;
+    const reference = this.productForm.value.reference;
     const product = this.products.find(product => product.reference === reference);
     if (product) {
+      this.isEditing = true;
       this.productForm.setValue({
         reference: product.reference,
         name: product.name,
@@ -133,31 +119,40 @@ export class DashboardComponent implements OnInit {
     this.ProductsService.shareData.set(this.products);
   }
 
-  // Método para cerrar el modal de éxito transcurridos 3 segundos
-  closeModal() {
-    setTimeout(() => {
-      this.success = false;
-    }, 3000);
+  ngOnInit(): void {
+    this.titleService.setTitle('Apple (UK) - Admin Dashboard');
+    this.products = this.ProductsService.shareData();
   }
 
   onSubmit() {
     // Verificamos si el formulario es válido antes de enviar los datos
     if (this.productForm.valid) {
-      // Asignamos los valores del formulario al objeto producto
-      Object.assign(this.product, this.productForm.value);
-      // Eliminamos la ruta del archivo y nos quedamos con el nombre del archivo
-      this.product.image = this.product.image.replace(/^.*[\\\/]/, '');
-      // Añadimos la ruta de la imagen al objeto de producto
-      this.product.image = './assets/images/' + this.product.image;
-      // Añadimos el nuevo producto al array de productos
-      this.products.push(this.product);
-      // Enviamos los datos al servicio
-      this.ProductsService.shareData.set(this.products);
-      // Mostramos el modal de éxito
-      this.success = true;
-      // Cerramos el modal de éxito transcurridos 3 segundos
-      this.closeModal();
-      // Reiniciamos el formulario
+      // Verificamos si estamos editando un producto o añadiendo uno nuevo
+      if (this.isEditing) {
+        const index = this.products.findIndex(product => product.reference === this.productForm.value.reference);
+        this.products[index] = this.productForm.value;
+      } else {
+        // Asignamos los valores del formulario al objeto producto
+        Object.assign(this.product, this.productForm.value);
+        // Eliminamos la ruta del archivo y nos quedamos con el nombre del archivo
+        this.product.image = this.product.image.replace(/^.*[\\\/]/, '');
+        // Añadimos la ruta de la imagen al objeto de producto
+        this.product.image = './assets/images/' + this.product.image;
+        // Añadimos el nuevo producto al array de productos
+        this.products.push(this.product);
+        // Enviamos los datos al servicio
+        this.ProductsService.shareData.set(this.products);
+      }
+      // Mostramos el popup
+      this.showPopup = true;
+      // Mostramos el mensaje en el popup dependiendo si estamos editando o añadiendo un producto
+      this.popupMessage = this.isEditing ? 'Producto editado correctamente' : 'Producto añadido correctamente';
+      // Cerramos el popup después de 3 segundos
+      setTimeout(() => {
+        this.showPopup = false;
+      }, 3000);
+      // Reiniciamos el formulario y el estado de edición
+      this.isEditing = false;
       this.productForm.reset();
     }
   }
