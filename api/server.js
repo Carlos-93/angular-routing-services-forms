bce320ecba73523a0e881a43b07711be6d2266ad
Carlos-93 <carlos93.bcn@hotmail.com>
2024-05-16 18:52:29 +0200

    chore: Update server.js to improve error handling and code organization

--- api/server.js
+++ api/server.js
@@ -6,23 +6,27 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
+// Routes
 const route = require('./routes/products');
-
 app.use('/api', route);
 
-app.use((req, res) => {
+// Not found handler
+app.use((req, res, next) => {
     res.status(404).send('Not Found');
 });
 
+// Error handler
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
 
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
