ae11714d7c50047bf6e4c7e02396dd3e03240179
Carlos-93 <carlos93.bcn@hotmail.com>
2024-05-19 02:36:44 +0200

    chore: Update image URLs in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: () => ({
-        'iphone-banner': "url('/assets/images/banner-iphone.webp')",
+        'iphone-banner': "url('/assets/images/banner-iphone.jpeg')",
         'macbook-banner': "url('/assets/images/banner-macbook.jpeg')",
-        'watch-banner': "url('/assets/images/banner-watch.webp')",
+        'watch-banner': "url('/assets/images/banner-watch.jpeg')",
         'ipad-banner': "url('/assets/images/banner-ipad.jpeg')",
         'airpods-banner': "url('/assets/images/banner-airpods.jpeg')"
       })
